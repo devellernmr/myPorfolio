90e1e8df4cfc95527f7310dcccd37fdee813d80b
LeoEller <devellernmr@gmail.com>
2025-03-13 17:51:05 -0300

    feat add section home and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Navbar from "./Pages/Navbar/Navbar"
+import Home from "./Pages/Home/Home"
 import "./App.css"
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   )
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,39 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <section className="homeSection" id="home">
+      <div className="homeDiv flex">
+        <div className="homeText">
+          <h1 className="homeMain">
+            Desenvolvedor
+            <br />
+            Front-End
+            <br />
+            <span>LeoEller</span>
+          </h1>
+          <p className="homeSecond">
+            Programador Front-End de 16 anos especializado em HTML, CSS,
+            JavaScript e React. Confira meus projetos!
+          </p>
+          <div className="imgDiv">
+          <img className="img-mobile" src="src/img/laptop+smartphone.png" alt="" />
+          </div>
+          <div className="buttonDiv flex">
+            <a href="#contact" className="button buttonContact">
+              Contate-me
+            </a>
+            <a href="#projects" className="button buttonProjects">
+              Projetos
+            </a>
+          </div>
+        </div>
+        <div className="imgDiv">
+            <img className="img-pc" src="src/img/laptop.png" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
